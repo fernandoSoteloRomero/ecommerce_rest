3aed5e8d63258078780569d370ba31604818c9a5
fernando sotelo <al169888@alumnos.uacj.mx>
2024-05-03 20:55:56 -0600

    Agregando logout para el usuario

--- apps/users/auth.py
+++ apps/users/auth.py
@@ -5,6 +5,7 @@ from rest_framework.authtoken.models import Token
 from apps.users.api.serializers import UserTokenSerializer
 from django.contrib.sessions.models import Session
 from datetime import datetime
+from rest_framework.views import APIView
 #hereda de api view
 class Login(ObtainAuthToken):
   #nos llegara el usuario y la contraseña
@@ -46,7 +47,32 @@ class Login(ObtainAuthToken):
 
 
 
-
+class Logout(APIView):
+    def post(self, request, *args, **kwargs):
+      print(request.data)
+      token = request.data.get('token')  # Cambio request.GET por request.data para obtener datos del cuerpo del POST
+      try:
+        token = Token.objects.get(key=token)
+      except Token.DoesNotExist:
+        return Response({'error': 'El token proporcionado no es válido'}, status=status.HTTP_404_NOT_FOUND)
+      
+      user = token.user
+      all_sessions = Session.objects.filter(expire_date__gte=datetime.now())
+      if all_sessions.exists():
+        for session in all_sessions:
+          session_data = session.get_decoded()
+          if user.id == int(session_data.get('_auth_user_id')):
+            session.delete()
+  
+        token.delete()
+        session_message = 'Sesiones de usuario eliminadas'
+        token_message = 'Token eliminado'
+        return Response({
+          'token_message': token_message, 
+          'session_message': session_message
+        }, status=status.HTTP_200_OK)
+      
+      return Response({'error': 'No se ha encontrado un usuario con estas credenciales'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 
--- ecommerce_rest/urls.py
+++ ecommerce_rest/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from apps.users.auth import Login
+from apps.users.auth import Login, Logout
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', Login.as_view(), name='Login'),
+    path('logout/', Logout.as_view(), name='Logout'),
     path( 'usuario/', include('apps.users.api.urls') ),
     path( 'products/', include('apps.products.api.urls') ),
     # YOUR PATTERNS
